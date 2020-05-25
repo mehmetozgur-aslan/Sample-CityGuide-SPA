36d8c6e00afe77162746a906167b042cb187f877
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-25 12:25:45 +0300

    value componenti api baglantisi

--- CityGuide-SPA/src/app/value/value.component.ts
+++ CityGuide-SPA/src/app/value/value.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Value } from '../models/value';
 
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  styleUrls: ['./value.component.css'],
 })
 export class ValueComponent implements OnInit {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
-
+  values: Value[] = [];
   ngOnInit(): void {
+    this.getValues().subscribe((data) => {
+      this.values = data;
+    });
   }
 
+  getValues() {
+    return this.httpClient.get<Value[]>('http://localhost:58822/api/values');
+  }
 }
